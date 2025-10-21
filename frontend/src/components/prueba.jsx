7f5ade68ad1d7f93446b8a50960974657acfcebd
PetusoTwo <1566258@senati.pe>
2025-10-21 12:32:34 -0500

    arcivo de pruebas

--- frontend/src/components/prueba.jsx
+++ frontend/src/components/prueba.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function prueba() {
   return (
     <div className='bg-blue-500 text-white p-4'>
-      <p className='text-5xl'>yo ke se</p>
+      <p className='text-5xl'>PetuPharmacy</p>
     </div>
   )
 }
