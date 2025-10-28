c2d0f8a7711213bc9bc4769ccbfeca0280fa265c
PetusoTwo <1566258@senati.pe>
2025-10-28 11:35:55 -0500

    mejroa front test

--- frontend/src/components/prueba.jsx
+++ frontend/src/components/prueba.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function prueba() {
   return (
-    <div className='bg-blue-500 text-white p-4'>
-      <p className='text-5xl'>PetuPharmacy</p>
+    <div className='bg-blue-500 text-white p-4 text-center'>
+      <p className='text-5xl font-black dark:text-white'>PetuPharmacy</p>
     </div>
   )
 }
